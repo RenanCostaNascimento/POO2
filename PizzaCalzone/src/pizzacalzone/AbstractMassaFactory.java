1259c194cdf1ee1a5812172a8924768601b2a071
Renan <Renan@192.168.1.3>
2014-04-03 22:12:42 -0300

    Commit inicial.

---
+++ PizzaCalzone/src/pizzacalzone/AbstractMassaFactory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pizzacalzone;
+
+/**
+ *
+ * @author Renan
+ */
+public abstract class AbstractMassaFactory {
+    
+    public abstract Presunto createPresunto();
+    public abstract Calabresa createCalabresa();
+}
